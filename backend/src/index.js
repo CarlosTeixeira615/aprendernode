91c97442e5ba20866b130d982b70163ef82c2474
CarlosTeixeira615 <carlosteixeira.tc@hotmail.com>
2020-07-23 23:46:42 -0300

    att

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,28 @@
 const express = require("express");
-const { uuid } = require("uuidv4");
+const { uuid, isUuid } = require("uuidv4");
 const app = express();
 app.use(express.json());
 
 const projects = [];
+function logReq(req, res, next){ 
+ const { method, url }= req
+ const logLabel = `[${method.toUpperCase()}] ${url}`;
+  console.log(logLabel)
+  return next()
+}
 
-app.get("/projects", (req, res) => {
+function validateProjetcIdm(req, res, next){
+   const { id } = req.params
+   if(!isUuid(id)){
+     return res.status(400).json({ error: 'Invalid project id'})
+   }
+   return next()
+}
+
+app.use(logReq)
+app.use('/projects/:id', validateProjetcIdm)
+
+app.get("/projects",logReq, (req, res) => {
     const { title } = req.query;
   const results = title
      ? projects.filter(project => project.title.includes(title))
@@ -13,14 +30,14 @@ app.get("/projects", (req, res) => {
   return res.json(results);
 });
 
-app.post("/projects", (req, res) => {
+app.post("/projects",  (req, res) => {
   const { title, owner } = req.body;
   const project = { id: uuid(), title, owner };
   projects.push(project);
   return res.json(project);
 });
 
-app.put("/projects/:id", (req, res) => {
+app.put("/projects/:id",  (req, res) => {
   const { id } = req.params;
   const { title, owner } = req.body;
   const projectIndex = projects.findIndex(project => project.id === id)
